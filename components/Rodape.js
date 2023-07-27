13563baf043b6b327f82c0bbfb8478ec23020c66
Lucas de Souza Vieira <dev.lucas.sv@gmail.com>
2023-07-27 01:31:03 -0300

    Footer

--- components/Rodape.js
+++ components/Rodape.js
@@ -1,6 +1,8 @@
+import styles from '../src/styles/Rodape.module.css'
+
 export default function Rodape(){
     return(
-        <footer>
+        <footer className={styles.rodape}>
             <p>
                 <span>&copy; JOJOS GAME STUDIO. ALL RIGHTS RESERVED</span>
             </p>
